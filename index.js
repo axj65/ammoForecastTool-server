882047528ea6f8f79a9eb4bfbb90a7f6c5adcaa9
axj65 <122125368+axj65@users.noreply.github.com>
2023-11-28 13:30:38 -0600

    undid changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 const cors = require("cors");
-//require("dotenv").config({ path: "./config.env" });
+require("dotenv").config({ path: "./config.env" });
 
 const port = process.env.PORT || 5050;
 app.use(cors({}));
@@ -10,11 +10,11 @@ app.use(express.json());
 app.use(require("./routes/routes")); 
 
 // get driver connection
-//const dbo = require("./database/conn");
+const dbo = require("./database/conn");
 
 app.listen(port, () => {
     // perform a database connection when server starts
-    //dbo.connectToServer();
+    dbo.connectToServer();
     console.log(`Server is running on port: ${port}`);
 });
 
